2f1cf0748df3ace52d1103bada785335ba3d9ee9
Siddharth Bansal <bsiddharth1302@gmail.com>
2023-04-02 18:30:52 +0530

    Dashboard Designed

--- app/src/main/java/com/google/app/biird/DashBoard.kt
+++ app/src/main/java/com/google/app/biird/DashBoard.kt
@@ -1,11 +1,44 @@
 package com.google.app.biird
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.app.biird.Fragements.HomeFrag
+import com.google.app.biird.Fragements.Profilefrag
+import com.google.app.biird.Fragements.SearchFrag
+import com.google.firebase.auth.FirebaseAuth
 
 class DashBoard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dash_board)
+        supportActionBar!!.hide()
+        val auth=FirebaseAuth.getInstance()
+        findViewById<ImageView>(R.id.logout).setOnClickListener {
+            auth.signOut()
+            startActivity(Intent(this,Login::class.java))
+            finish()
+        }
+        supportFragmentManager.beginTransaction().add(R.id.mainFrame,HomeFrag()).commit()
+        findViewById<BottomNavigationView>(R.id.bottomNavigationView).setOnNavigationItemSelectedListener {item->
+        when(item.itemId){
+            R.id.home->{
+                supportFragmentManager.beginTransaction().replace(R.id.mainFrame,HomeFrag()).commit()
+                true
+            }
+            R.id.search->{
+                supportFragmentManager.beginTransaction().replace(R.id.mainFrame,SearchFrag()).commit()
+                true
+            }
+            R.id.profile->{
+                supportFragmentManager.beginTransaction().replace(R.id.mainFrame,Profilefrag()).commit()
+                true
+            }
+            else ->false
+        }
+
+        }
     }
 }
---
+++ app/src/main/java/com/google/app/biird/Fragements/HomeFrag.kt
@@ -0,0 +1,18 @@
+package com.google.app.biird.Fragements
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.app.biird.R
+class HomeFrag : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view= inflater.inflate(R.layout.fragment_home, container, false)
+        return view
+    }
+}
